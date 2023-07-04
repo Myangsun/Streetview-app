343e4138355da9a74468bfde7dfb9aae4d640e94
Mingyang Sun <myangsun@outlook.com>
2023-07-04 01:51:35 -0400

    react complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-// import MapComponent from "./components/MapContainer";
-// import MapContent from "./components/MapContent";
-import MapComponent from "./components/Maptest";
+import MapComponent from "./components/MapComponent";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App h-screen w-screen bg-gray-100 overflow-auto">
       <Header />
-      {/* <MapComponent /> */}
-      {/* <MapContent /> */}
       <MapComponent />
     </div>
   );
