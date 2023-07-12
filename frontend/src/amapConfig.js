1278c029017365254c2503e983e187e93da7ea98
Mingyang Sun <myangsun@outlook.com>
2023-07-12 13:28:24 -0400

    fix git 2

--- frontend/src/amapConfig.js
+++
@@ -1,6 +0,0 @@
-const amapConfig = {
-  securityJsCode: "5e0c8daa97f90c952d78ffd79f5a65a9",
-  key: "5006aba1c28995bc84672dca708fc2d8",
-};
-
-export default amapConfig;
--- frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-import AWS from "aws-sdk";
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient({
-  region: "us-east-2",
-  accessKeyId: "AKIA4LHNCGVE7PY6KLHS",
-  secretAccessKey: "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",
-});
-
-export default dynamoDB;
